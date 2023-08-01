78a3d5f78cc29ac62a76c126f7c70495e0d6aa1a
Ogunbanjo Emmanuel <emmanuelogunbanjo8@gmail.com>
2023-08-01 21:14:54 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('#check-menu').on('click', function(){
+    const images = $('#images').position().top;
+    
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },
+        900
+    );
+});
